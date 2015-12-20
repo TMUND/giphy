47acf40c862acc67b9cc75941953debf42456b34
Toni Munder <tonimunder@hotmail.com>
2015-12-20 07:35:10 -0800

    Initial commit

---
+++ main.js
@@ -0,0 +1,15 @@
+(function() {
+
+  window.Main = {
+    // ----------
+    init: function() {
+    	
+    }
+  };
+
+  // ----------
+  $(document).ready(function() {
+    Main.init();
+  });
+
+})();
